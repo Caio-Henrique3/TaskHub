8e1c808ef9cf48245201196970a46c1777c44da7
Caio Henrique <caioaranha@hotmail.com>
2025-06-03 19:31:10 -0300

    feat: implement authentication and user login functionality

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,15 +1,16 @@
 import { Application, Router } from "express";
 import { UserController } from "../controllers/userController";
 import { validateUser } from "../middlewares/validateUser";
+import { authenticateToken } from "../middlewares/authMiddleware";
 
 export function userRoutes(app: Application) {
   const router = Router();
 
   router.get("/", UserController.findAll);
-  router.get("/:id", UserController.findById);
+  router.get("/:id", authenticateToken, UserController.findById);
   router.post("/", validateUser, UserController.register);
-  router.put("/:id", validateUser, UserController.update);
-  router.delete("/:id", UserController.delete);
+  router.put("/:id", authenticateToken, validateUser, UserController.update);
+  router.delete("/:id", authenticateToken, UserController.delete);
 
   app.use("/users", router);
 }
