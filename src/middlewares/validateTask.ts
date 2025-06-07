b1ac71599ea5a1866fb3293b3e53eeab3cf1e1a2
Caio Henrique <71612389+Caio-Henrique3@users.noreply.github.com>
2025-06-06 23:46:33 -0300

    fix: incorrect word correction

--- src/middlewares/validateTask.ts
+++ src/middlewares/validateTask.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 export function validateTask(req: Request, res: Response, next: NextFunction) {
   const { title, description, status } = req.body;
   if (!title || !description || !status) {
-    res.status(400).json({ message: "Títu, descrição e status são obrigatórios." });
+    res.status(400).json({ message: "Título, descrição e status são obrigatórios." });
 
     return;
   }
