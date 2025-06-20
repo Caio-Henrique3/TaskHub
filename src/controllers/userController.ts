4a2e6ff63a7f17db9ebd4268ce17ba8c4e14ef89
Caio Henrique <caioaranha@hotmail.com>
2025-06-20 15:02:20 -0300

    feat: added swagger documentation for users endpoint

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,72 +1,129 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/userService";
+import { NotFoundError, ValidationError } from "../utils/errors";
+import mongoose from "mongoose";
+import { getPagination } from "../utils/pagination";
 
+function buildUserFilter(query: any) {
+  const filters: any = { ...query };
+
+  if (filters.email) {
+    filters.email = { $regex: filters.email, $options: "i" };
+  }
+
+  return filters;
+}
 export class UserController {
-  static async findAll(request: Request, response: Response) {
+  static async findAll(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     try {
-      response.json(await UserService.findAll());
+      const filters = buildUserFilter(request.query);
+
+      const { page, limit, skip } = getPagination(filters);
+
+      const [users, total] = await Promise.all([
+        UserService.findAll(filters, limit, skip),
+        UserService.count(filters),
+      ]);
+
+      response.send({
+        data: users,
+        page,
+        limit,
+        total,
+        totalPages: Math.ceil(total / limit),
+      });
     } catch (error: any) {
-      response.status(500).json({ message: "Erro ao buscar usuários." });
+      next(error);
     }
   }
 
-  static async findById(request: Request, response: Response) {
+  static async findById(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     try {
       const user = await UserService.findById(request.params.id);
       if (!user) {
-        response.status(404).json({ message: "Usuário não encontrado." });
-
-        return;
+        throw new NotFoundError(
+          `Usuário com id ${request.params.id} não encontrado.`
+        );
       }
 
-      response.json(user);
+      response.send(user);
     } catch (error: any) {
-      response.status(500).json({ message: "Erro ao buscar usuário." });
+      if (error instanceof mongoose.Error.CastError) {
+        throw new ValidationError(`Id ${error.value} é inválido.`);
+      }
+
+      next(error);
     }
   }
 
-  static async register(request: Request, response: Response) {
+  static async register(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     const { email, password } = request.body;
     try {
       const user = await UserService.register(email, password);
-      response
-        .status(201)
-        .json({ message: "Usuário registrado com sucesso.", userId: user._id });
+
+      response.status(201).send();
     } catch (error: any) {
-      response.status(400).json({ message: error.message });
+      next(error);
     }
   }
 
-  static async update(request: Request, response: Response) {
+  static async update(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     try {
-      const user = await UserService.findById(request.params.id);
-      if (!user) {
-        response.status(404).json({ message: "Usuário não encontrado." });
-
-        return;
+      const userUpdated = await UserService.update(
+        request.params.id,
+        request.body
+      );
+      if (!userUpdated) {
+        throw new NotFoundError(
+          `Usuário com id ${request.params.id} não encontrado.`
+        );
       }
 
-      await UserService.update(request.params.id, request.body);
-      response.json({
-        message: "Usuário atualizado com sucesso.",
-      });
+      response.send(userUpdated);
     } catch (error: any) {
-      response.status(400).json({ message: error.message });
+      if (error instanceof mongoose.Error.CastError) {
+        throw new ValidationError(`Id ${error.value} é inválido.`);
+      }
+
+      next(error);
     }
   }
 
-  static async delete(request: Request, response: Response) {
+  static async delete(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     try {
-      const user = await UserService.findById(request.params.id);
-      if (!user) {
-        response.status(404).json({ message: "Usuário não encontrado." });
-        return;
+      if (!(await UserService.delete(request.params.id))) {
+        throw new NotFoundError(
+          `Usuário com id ${request.params.id} não encontrado.`
+        );
       }
 
-      await UserService.delete(request.params.id);
       response.status(204).send();
     } catch (error: any) {
-      response.status(500).json({ message: "Erro ao deletar usuário." });
+      if (error instanceof mongoose.Error.CastError) {
+        throw new ValidationError(`Id ${error.value} é inválido.`);
+      }
+
+      next(error);
     }
   }
 }
